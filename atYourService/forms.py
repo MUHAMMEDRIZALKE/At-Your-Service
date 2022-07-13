9596900def9e0dd754322f55607887c610622fe7
MUHAMMED RIZAL KE <muhammedrizalke@gmail.com>
2022-07-13 21:44:45 +0530

    added more choice to the profession selection and other small changes

--- atYourService/forms.py
+++ atYourService/forms.py
@@ -3,8 +3,16 @@ from leaflet.forms.widgets import LeafletWidget
 
 
 class UserForm(forms.Form):
-    jobs = (('mechanic', 'MECHANIC'),
-            ('electrician', 'Electrician'))
+    jobs = (
+        ('mechanic', 'MECHANIC'),
+        ('electrician', 'ELECTRICIAN'),
+        ('plumber', 'PLUMBER'),
+        ('domestic Worker', 'DOMESTIC WORKER'),
+        ('cook', 'COOK'),
+        ('driver', 'DRIVER'),
+        ('gardener', 'GARDENER'),
+        ('saloon', 'SALOON'),
+    )
 
     Profession = forms.CharField(widget=forms.Select(choices=jobs))
     location = forms.PointField(widget=LeafletWidget())
